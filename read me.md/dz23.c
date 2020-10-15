52c4fa0b120880588dfc692100a4e72dd1be7046
suleiman1404 <56249020+suleiman1404@users.noreply.github.com>
2020-10-15 03:56:11 +0300

    Update dz23.c

--- read me.md/dz23.c
+++ read me.md/dz23.c
@@ -16,6 +16,7 @@ void push(node_t** head, int vertice1, int vertice2) {
     (*head) = tmp;
 }
 
+/* ЗАПИСЬ СВЯЗЕЙ В ФАЙЛ */
 void print_to_file(node_t* head, FILE* file) {
     fprintf(file, "graph graphname {\n");
     while(head != NULL) {
@@ -30,6 +31,7 @@ void print_to_file(node_t* head, FILE* file) {
     fprintf(file, "}");
 }
 
+/* ДОБАВЛЕНИЕ СВЯЗЕЙ МЕЖДУ ВЕРШИНАМИ */
 void add_edges(node_t** head) {
     while(1) {
         char c;
@@ -40,20 +42,24 @@ void add_edges(node_t** head) {
         int m = 1;
         int k, f;
 
+        /* СЧИТЫВАНИЕ ИЗ КОНСОЛИ*/
         char str[7];
         while((c = getchar()) != '\n') {
             str[i] = c;
             i++;
         }
 
+        /* ПРЕКРАЩЕНИЕ СЧИТЫВАНИЯ РЁБЕР*/
         if(str[0] == '-' && str[1] == '1') {
             break;
         }
 
+        /* НАЙТИ ПОЗИЦИИ РАЗДЕЛИТЕЛЯ*/
         while(str[j] != '-') {
             j++;
         }
 
+        /* ПОЛУЧЕНИЕ ПЕРВОЙ ВЕРШИНЫ*/
         k = j-1;
         while(k != -1) {
             a = a + m*(str[k] - '0');
@@ -69,12 +75,29 @@ void add_edges(node_t** head) {
             m = 10*m;
             f--;
         }
-        //printf("b = %d\n", b);
-
         push(head, a, b);
     }
 }
 
+/* ПРОВЕРКА НА ПЕТЛИ */
+int loops_counter(node_t* head) {
+    int l = 0;
+    while(head != NULL) {
+        if(head->vertice1 == head->vertice2) l++;
+        head = head->next;
+    }
+    return l;
+}
+
+/* ПРОВЕРКА НА КРАТНЫЕ РЁБЕРА */
+int multiple_edges_counter(node_t* head) {
+    node_t* tmp = head;
+    int n = 0;
+    while (head != NULL) {
+        if(head->vertice2 != 0) n++;
+        head = head->next;
+    }
+    
     int arr[2][n];
     int i = 0;
     while(tmp != NULL) {
@@ -119,7 +142,7 @@ int vertice_counter(node_t* head) {
         n++;
         head = head->next;
     }
-    
+
     int arr[n*2];
     for(int i = 0; i < n*2; i = i + 2) {
         arr[i] = tmp->vertice1;
@@ -135,12 +158,21 @@ int vertice_counter(node_t* head) {
         }
     }
 
-
     int k = 0;
     for(int i = 0; i < n*2; i++) {
         if(arr[i] != -1 && arr[i] != 0) k++;
     }
     return k;
+}
+
+/* ПРОВЕРКА НА СВЯЗНОСТЬ */
+int connectivity_check(int v, int e, node_t* head) {
+    if(loops_counter(head) || multiple_edges_counter(head) != 0) {
+        return -2;
+    } else if(e > (v-1)*(v-2)*0.5) {
+        return 1;
+    } else 
+        return -1;
 }
 
 int main(void)
@@ -153,18 +185,31 @@ int main(void)
         printf("Error opening file\n");
         return -1;
     }
+
     printf("Вводите по типу a-a\n");
     printf("Чтобы остановить, введите -1\n");
     printf("Чтобы добавить изолированную вершину b-\n");
-    
+
     add_edges(&head);
+
     print_to_file(head, file);
+
     fclose(file);
 
     printf("\nEdges = %d\n", edge_counter(head));
     printf("Vertices = %d\n", vertice_counter(head));
     printf("Loops = %d\n", loops_counter(head));
     printf("Multiple edges = %d\n\n", multiple_edges_counter(head));
+
+    if (connectivity_check(vertice_counter(head), edge_counter(head), head) == 1)
+        printf("This is a connected graph\n");
+    else if(connectivity_check(vertice_counter(head), edge_counter(head), head) == -1)
+        printf("This is a DISconnected graph\n");
+    else
+        printf("This is not a simple graph!\n");
+
+    system("dot -Tpng graph.dot -o graph.png");
+    system("graph.png");
 
     return 0;
 }
