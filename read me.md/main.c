26ebff95d3b1e66be6e7bbd22fc60077fa5764fc
suleiman1404 <56249020+suleiman1404@users.noreply.github.com>
2020-10-15 04:42:59 +0300

    Update main.c

--- read me.md/main.c
+++ read me.md/main.c
@@ -1,85 +1,173 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 100
 
-int *push(int *array, int *num, int elem) {
-  if(*num < MAX){
-    array[*num] = elem;
-    *num += 1;
-      return array;
-  } else {
-    printf("error: num > MAX\n");
-      return array;
-  }
+typedef struct node {
+    int point1;
+    int point2;
+    struct node* next;
+} node_t;
+
+void push(node_t** head, int point1, int point2) {
+    node_t* tmp = (node_t*)calloc(sizeof(node_t), 1);
+    if(tmp == NULL) return;
+    tmp->point1 = point1;
+    tmp->point2 = point2;
+    tmp->next = (*head);
+    (*head) = tmp;
 }
 
-int pop(int *array, int *num) {
-  int array2;
-  
-  if(*num > 0){
-      *num -= 1;
-      array2 = array[*num];
-    return array2;
-  } else {
-    printf("there is no info store\n");
-    return 0;
-  }
-}
-
-int isempty(int array[MAX], int num) {
-  if(num == 0)
-  return 1;
-  
-  else
-  return 0;
-  
-}
-
-void stkPrint(int array[MAX], int num) {
-  int i = num;
-  
-  if(isempty(array, num) == 1)
-      return;
-  
-  while(i>0){
-    printf("%d\n", array[--i]);
-  }
-}
-
-int main(void) {
-  int *array = calloc(MAX, sizeof(int));
-  int num=0;
-  int vibor = 0;
-  int elem;
-    
-      while(vibor!=4){
-          printf("1)ADD\n2)DELETE\n3)PRINT\n4)exit\nCHOOSE: ");
-    
-          scanf("%d", &vibor);
-  
-  switch(vibor){
-            case 1:
-                printf("enter arrayent:");
-                scanf("%d", &elem);
-                array = push(array, &num, elem);
-            break;
-            
-            case 2:
-                printf("delete %d\n", pop(array, &num));
-            break;
-            
-            case 3:
-                stkPrint(array, num);
-            break;
-            
-            case 4:
-                exit(0);
-            break;
-  
+void print_to_file(node_t* head, FILE* file) {
+    fprintf(file, "ГРАФ {\n");
+    while(head != NULL) {
+        fprintf(file, "%d", head->point1);
+        if(head->point2 != 0) {
+            fprintf(file, " -- ");
+            fprintf(file, "%d", head->point2);
         }
+        fprintf(file, ";\n");
+        head = head->next;
+    }
+    fprintf(file, "}");
+}
 
+void add_edges(node_t** head) {
+    while(1) {
+        char c;
+        int a = 0;
+        int b = 0;
+        int i = 0;
+        int j = 0;
+        int m = 1;
+        int k, f;
+
+        char str[7];
+        while((c = getchar()) != '\n') {
+            str[i] = c;
+            i++;
+        }
+        if(str[0] == '-' && str[1] == '1') {
+            break;
+        }
+        while(str[j] != '-') {
+            j++;
+        }
+        k = j-1;
+        while(k != -1) {
+            a = a + m*(str[k] - '0');
+            m = 10*m;
+            k--;
+        }
+        f = i-1;
+        m = 1;
+        while(f != j) {
+            b = b + m*(str[f] - '0');
+            m = 10*m;
+            f--;
+        }
+        push(head, a, b);
+    }
+}
+int loops_counter(node_t* head) {
+    int l = 0;
+    while(head != NULL) {
+        if(head->point1 == head->point2) l++;
+        head = head->next;
+    }
+    return l;
+}
+
+int multiple_edges_counter(node_t* head) {
+    node_t* tmp = head;
+    int n = 0;
+    while (head != NULL) {
+        if(head->point2 != 0) n++;
+        head = head->next;
     }
     
-    return 0;
+    int arr[2][n];
+    int i = 0;
+    while(tmp != NULL) {
+        arr[0][i] = tmp->point1;
+        arr[1][i] = tmp->point2;
+        i++;
+        tmp = tmp->next;
+    }
+
+    int m = 0;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if ((arr[0][i] == arr[0][j]) && (arr[1][i] == arr[1][j])) {
+                m++;
+            } else if((arr[0][i] == arr[1][j]) && (arr[1][i] == arr[0][j])) {
+                m++;
+            }
+            
+        }
+    }
+    return m;
+}
+
+int edge_counter(node_t* head) {
+    int n = 0;
+    while(head != NULL) {
+        if(head->point2 != 0) {
+            n++;
+        }
+        head = head->next;
+    }
+    return n;
+}
+
+int point_counter(node_t* head) {
+    int n = 0;
+    node_t* tmp = head;
+    while(head != NULL) {
+        n++;
+        head = head->next;
+    }
+    int arr[n*2];
+    for(int i = 0; i < n*2; i = i + 2) {
+        arr[i] = tmp->point1;
+        arr[i+1] = tmp->point2;
+        tmp = tmp->next;
+    }
+    for(int i = 0; i < n*2 - 1; i++) {
+        for(int j = i + 1; j < n*2; j++) {
+            if(arr[i] != -1 && arr[i] == arr[j]) {
+                arr[j] = -1;
+            }
+        }
+    }
+    int k = 0;
+    for(int i = 0; i < n*2; i++) {
+        if(arr[i] != -1 && arr[i] != 0) k++;
+    }
+    return k;
+}
+
+int main(void)
+{
+    node_t* head = NULL;
+
+    FILE *file;
+    file = fopen("graph.dot", "w");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return -1;
+    }
+
+    printf("Вводите по типу a-a\n");
+    printf("Чтобы остановить, введите -1\n");
+    printf("Чтобы добавить изолированную вершину b-\n");
     
+    add_edges(&head);
+    print_to_file(head, file);
+    fclose(file);
+
+    printf("\nEdges = %d\n", edge_counter(head));
+    printf("points = %d\n", point_counter(head));
+    printf("Loops = %d\n", loops_counter(head));
+    printf("Multiple edges = %d\n\n", multiple_edges_counter(head));
+
+    return 0;
 }
